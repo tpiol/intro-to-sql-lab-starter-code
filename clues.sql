1f35a645708a1720856187818ee26ee6844d4f5a
Tia Piol <tiarox02@yahoo.com>
2025-09-05 09:14:11 -0700

    exercise 2

--- clues.sql
+++ clues.sql
@@ -1,11 +1,14 @@
+/c world
+
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
  
--- Write SQL query here
+select code, name, population from countries where region = 'Southern Europe' order by population asc limit 1;
+
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
--- Write SQL query here
+select language from countrylanguages where countrycode = 'VAT';
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
