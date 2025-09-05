9e1db23fdfa88356fe148815ac9bb8184233ec8f
Tia Piol <tiarox02@yahoo.com>
2025-09-05 09:42:35 -0700

    exercise 6

--- clues.sql
+++ clues.sql
@@ -30,7 +30,7 @@ select name, countrycode from cities where name like 'Serra%' and countrycode !=
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
 
--- Write SQL query here
+select * from cities where countrycode = 'BRA' and id = 211;
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
