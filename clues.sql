c8f968c6f483ad75cd79c434eea1531938623607
Tia Piol <tiarox02@yahoo.com>
2025-09-05 09:23:21 -0700

    exercise 3

--- clues.sql
+++ clues.sql
@@ -13,7 +13,7 @@ select language from countrylanguages where countrycode = 'VAT';
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
--- Write SQL query here
+select * from countrylanguages where language = 'Italian' and percentage = 100;
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
